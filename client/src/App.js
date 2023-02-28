51add97878e8eecd83da5bd831c9107eb6f19c41
Andrew Milner <95140821+milnera92@users.noreply.github.com>
2023-02-28 14:55:16 -0500

    set up project, code for fetch merchant data

---
+++ client/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Table = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  th,
+  td {
+    border: 1px solid black;
+    padding: 8px;
+    text-align: left;
+  }
+  th {
+    background-color: #f2f2f2;
+  }
+`;
+
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:8000/api.php");
+      const jsonData = await response.json();
+      setData(jsonData);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Merchant Transactions</h1>
+      {data ? (
+        <Table>
+          <thead>
+            <tr>
+              <th>Merchant ID</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.merchants.map((merchant) => (
+              <tr key={merchant.merchant_id}>
+                <td>{merchant.merchant_id}</td>
+                <td>{merchant.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </div>
+  );
+};
+
+export default App;
