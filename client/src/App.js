aae828433ceeaf0456345fe756b78dd8fe3d26bd
Andrew Milner <95140821+milnera92@users.noreply.github.com>
2023-02-28 17:17:58 -0500

    fetch from php server works and displays data

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const Table = styled.table`
-  border-collapse: collapse;
-  width: 100%;
-  th,
-  td {
-    border: 1px solid black;
-    padding: 8px;
-    text-align: left;
-  }
-  th {
-    background-color: #f2f2f2;
-  }
-`;
+// const Table = styled.table`
+//   border-collapse: collapse;
+//   width: 100%;
+//   th,
+//   td {
+//     border: 1px solid black;
+//     padding: 8px;
+//     text-align: left;
+//   }
+//   th {
+//     background-color: #f2f2f2;
+//   }
+// `;
 
 const App = () => {
   const [data, setData] = useState(null);
@@ -27,31 +27,18 @@ const App = () => {
     fetchData();
   }, []);
 
-console.log(data);
+  console.log(data.merchants[0].name);
+
+if (!data) {
+  return <div>Loading...</div>;
+}
 
   return (
     <div>
-      <h1>Merchant Transactions</h1>
-      {data ? (
-        <Table>
-          <thead>
-            <tr>
-              <th>Merchant ID</th>
-              <th>Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.merchants.map((merchant) => (
-              <tr key={merchant.merchant_id}>
-                <td>{merchant.merchant_id}</td>
-                <td>{merchant.name}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      ) : (
-        <p>Loading data...</p>
-      )}
+      <h1>Merchants:</h1>
+      {data.merchants.map((merchant) => (
+        <div key={merchant.id}>{merchant.name}</div>
+      ))}
     </div>
   );
 };
